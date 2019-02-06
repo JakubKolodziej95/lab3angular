ab0563caa6eb2d34ba6bcc1189511c099cceea3a
JakubKolodziej95 <37178990+JakubKolodziej95@users.noreply.github.com>
2019-02-06 15:47:52 +0000

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'root-directive',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'first-app';
+  name = 'Thomas';
+  age = '53';
+}
